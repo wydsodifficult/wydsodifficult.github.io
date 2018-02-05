7abff17f8e2aa8ed0140f44b5e71845cddbc35af
brandon higashi <Higashi@brandons-MBP.hawaii.rr.com>
2018-02-04 19:55:38 -1000

    Added in removal of missing localStorage.

--- js/app.js
+++ js/app.js
@@ -2550,6 +2550,9 @@ $('#logout').add('#logout-sidebar').on('click', function() {
         localStorage.removeItem('WYDuserCompanyID');
         localStorage.removeItem('WYDuserNumID');
         localStorage.removeItem('WYDuserID');
+        localStorage.removeItem('WYDviewClicked');
+        localStorage.removeItem('WYDuserUnused');
+        localStorage.removeItem('WYDadditionalEdit');
         console.log("Logging Out");
         //alert("You Have Successfully Signed Out!");
         window.location="index.html";
